7153cc05333cb7ff92331f6aebf7bc0c47732ec1
Sunattu <valentyn454@gmail.com>
2025-02-28 18:46:02 +0200

    додан віддільний об'єкт для звуків падіння + завтра буду робитиескейп меню

--- Assets/scripts/audio/Sound_Effect.cs
+++ Assets/scripts/audio/Sound_Effect.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class Sound_Effect : MonoBehaviour
 {
     public AudioClip soundClip;  // Àóä³îêë³ï, ÿêèé áóäå â³äòâîðþâàòèñÿ
-    private AudioSource audioSource;  // Êîìïîíåíò AudioSource
+    public AudioSource audioSource;  // Êîìïîíåíò AudioSource
+
 
     void Start()
     {
         // Îòðèìóºìî êîìïîíåíò AudioSource
-        audioSource = GetComponent<AudioSource>();
+        audioSource = GameObject.Find("Звук падіння")?.GetComponent<AudioSource>();
+        
     }
     private void OnCollisionEnter(Collision collision)
     {
